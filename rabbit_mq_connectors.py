b38411396ccf5e1650aaac94e9276a2689adfee2
kuptsovsasha <kuptsovsasha@gmail.com>
2023-05-15 09:36:32 +0300

    increase heartbeat time for redisMQ

--- rabbit_mq_connectors.py
+++ rabbit_mq_connectors.py
@@ -9,8 +9,8 @@ class RabbiMQConnectors:
         self.connection = self.__connect_to_rabbit_service()
 
     def __connect_to_rabbit_service(self):
-        parameters = pika.URLParameters(self.url)
-        connection = pika.BlockingConnection(parameters)
+        pika_url = self.url + "?heartbeat=360"
+        connection = pika.BlockingConnection(pika.URLParameters(pika_url))
         return connection
 
     def create_new_queue(self, queue_name: str):
